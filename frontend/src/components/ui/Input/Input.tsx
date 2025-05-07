207dd1f610da871707f3f90b39e218304263e8e9
golfstrimmar <yushinbox@gmail.com>
2025-05-07 11:55:43 +0200

    +

--- frontend/src/components/ui/Input/Input.tsx
+++ frontend/src/components/ui/Input/Input.tsx
@@ -76,3 +76,17 @@ const Input: React.FC<InputProps> = ({
 };
 
 export default Input;
+
+{
+  /* <Input
+  typeInput="text"
+  id="name"
+  data="Name"
+  name="name"
+  value={name}
+  onChange={(e) => setName(e.target.value)}
+  inputRef={inputRef}
+  onClick={handleClick}
+  activ={activ}
+/> */
+}
