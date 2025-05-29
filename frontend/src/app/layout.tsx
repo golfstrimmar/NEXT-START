aa3a0757515d1294cc96f0d5388a2ed6fdfe5a60
golfstrimmar <yushinbox@gmail.com>
2025-05-29 11:31:03 +0200

    bg

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-blue-100 text-gray-900`}
       >
         <GoogleOAuthProvider clientId={googleClientId}>
           <ClientWrapper>
