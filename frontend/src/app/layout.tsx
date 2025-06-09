408e1451df475a4d1d543b085ac77e533d838a7f
golfstrimmar <yushinbox@gmail.com>
2025-06-09 21:31:56 +0200

    ++

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 import Navbar from "@/components/Navbar/Navbar";
 import ClientWrapper from "@/components/ClientWrapper";
 import { GoogleOAuthProvider } from "@react-oauth/google";
